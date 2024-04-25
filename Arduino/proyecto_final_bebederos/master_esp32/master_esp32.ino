cfd04b6ea9fd8f75ac6f81dbb12cdc0a0b85f0d6
sabrinabaungartner <sabrinabaun@gmail.com>
2024-04-25 19:46:06 -0300

    se realizaron mas divisiones en las interrupciones y se agrego un todo list para generalizar bebederos

--- Arduino/proyecto_final_bebederos/master_esp32/master_esp32.ino
+++ Arduino/proyecto_final_bebederos/master_esp32/master_esp32.ino
@@ -6,14 +6,16 @@
 
 // Variables
 int seconds = 0;
+uint8_t water_value = 0;
+float water_temperature = 0.0;
 
 // Declaration of functions
 void iniciar_timer();
 void funcion_timer();
-void update_status_cattle_waterer(int water_value, float water_temperature);
+void update_status_cattle_waterer();
 
-void update_status_cattle_waterer(int water_value, float water_temperature) {
-  set_current_water_level_value(water_value);
+void update_status_cattle_waterer() {
+  set_current_water_level_value((int) water_value);
   set_current_water_temperature_value(water_temperature);
   set_current_date();
 }
@@ -28,19 +30,20 @@ void funcion_timer() {
   }
 
   if (seconds == 15) {
-    check_wifi();
     //get_user_selected();
     //get_cattle_waterer_selected();
-    //request_water_level();
     request_water_level_temperature();
     receive_water_level_temperature();
-    uint8_t water_value = receive_requested_water_level();
-    float water_temperature = receive_requested_water_temperature();
+    water_value = receive_requested_water_level();
+    water_temperature = receive_requested_water_temperature();
     Serial.print("water level value: ");
     Serial.println(water_value);
     Serial.print("water level temperature: ");
     Serial.println(water_temperature);
-    update_status_cattle_waterer((int) water_value, water_temperature);
+  }
+
+  if (seconds == 20) {
+    update_status_cattle_waterer();
     seconds = 0;
   }
 
